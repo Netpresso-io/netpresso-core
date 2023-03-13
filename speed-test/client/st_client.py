1c9da89f93c5696430455bedc86392c3c50738de
Omri Yoffe <omriyoffe366@gmail.com>
2023-03-13 15:55:03 +0200

    configure auto-init of influxdb

--- speed-test/client/st_client.py
+++ speed-test/client/st_client.py
@@ -1,51 +1,26 @@
 import speedtest
-import urllib.request
-import os
 import time
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-def measure_upload(url):
-    # Set up the file to upload and download
-    file_size = 1024 * 1024 * 1 # 1 MB
-    file_data = os.urandom(file_size)
-
-    # Measure upload bandwidth
-    start_time = time.time()
-    req = urllib.request.Request(url, data=file_data, method='PUT')
-    with urllib.request.urlopen(req) as f:
-        response = f.read()
-    end_time = time.time()
-    upload_speed = file_size / (end_time - start_time) / 1024 / 1024 # Mbps
-
-    # Return the results
-    return upload_speed
-
-def measure_download(url):
-    file_size = 1024 * 1024  # 1 MB
-    file_data = os.urandom(file_size)
-
-    # Measure download bandwidth
-    start_time = time.time()
-    urllib.request.urlretrieve(url, 'download.bin')
-    end_time = time.time()
-    download_speed = file_size / (end_time - start_time) / 1024 / 1024  # Mbps
-
-    # Delete the temporary files
-    os.remove('download.bin')
-
-    # Return the results
-    return download_speed
+# TODO: switch to secret on beta release
+INFLUX_URL = "http://influxdb:8086"
+INFLUX_TOKEN = "admin-token"
+INFLUX_ORG = "Netpresso"
+INFLUX_BUCKET = "speed-test-metrics"
 
 if __name__ == '__main__':
+    client = influxdb_client.InfluxDBClient(
+        url=INFLUX_URL,
+        token=INFLUX_TOKEN,
+        org=INFLUX_ORG
+    )
+    write_api = client.write_api(write_options=SYNCHRONOUS)
     st = speedtest.Speedtest()
     running = True
     while running:
-        print(str(st.upload())) # instead printing, add to DB with timestamp
-        print(str(st.download()))
-        time.sleep(60 * 15)
-
-    # url_download = 'http://localhost:5000/download'
-    # url_upload = 'http://localhost:5000/upload'
-    # upload_speed = measure_upload(url_upload)
-    # download_speed = measure_download(url_download)
-    # print(f'Download speed: {download_speed:.2f} Mbps')
-    # print(f'Upload speed: {upload_speed:.2f} Mbps')
+        download_speed = round(st.download() / 1024 / 1024, 2)
+        print(download_speed)
+        p = influxdb_client.Point("speed-test").field("speed", download_speed)
+        write_api.write(bucket=INFLUX_BUCKET, record=p)
+        time.sleep(15)
