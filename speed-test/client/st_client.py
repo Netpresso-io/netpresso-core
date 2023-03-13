28ad874185747066d62c3e364e3e4bc649dd1f46
talbeja <101974002+talbeja@users.noreply.github.com>
2023-03-13 13:33:40 +0200

    speed test against external server, with intervals

--- speed-test/client/st_client.py
+++ speed-test/client/st_client.py
@@ -1,7 +1,7 @@
+import speedtest
 import urllib.request
 import os
 import time
-
 
 def measure_upload(url):
     # Set up the file to upload and download
@@ -19,7 +19,6 @@ def measure_upload(url):
     # Return the results
     return upload_speed
 
-
 def measure_download(url):
     file_size = 1024 * 1024  # 1 MB
     file_data = os.urandom(file_size)
@@ -36,11 +35,17 @@ def measure_download(url):
     # Return the results
     return download_speed
 
-
 if __name__ == '__main__':
-    url_download = 'http://localhost:5000/download'
-    url_upload = 'http://localhost:5000/upload'
-    upload_speed = measure_upload(url_upload)
-    download_speed = measure_download(url_download)
-    print(f'Download speed: {download_speed:.2f} Mbps')
-    print(f'Upload speed: {upload_speed:.2f} Mbps')
+    st = speedtest.Speedtest()
+    running = True
+    while running:
+        print(str(st.upload())) # instead printing, add to DB with timestamp
+        print(str(st.download()))
+        time.sleep(60 * 15)
+
+    # url_download = 'http://localhost:5000/download'
+    # url_upload = 'http://localhost:5000/upload'
+    # upload_speed = measure_upload(url_upload)
+    # download_speed = measure_download(url_download)
+    # print(f'Download speed: {download_speed:.2f} Mbps')
+    # print(f'Upload speed: {upload_speed:.2f} Mbps')
