ef6d5947d0329eefb4ce7bc732f4d6f88d61bb4d
talbeja <talbeja05@gmail.com>
2023-05-11 16:04:03 +0300

    added 'packet_examples.txt', which is a readable example of how scapy packet objects look.test_scapy.py should now display top  accessed domains in a .pcap file.

--- hack/test_scapy.py
+++ hack/test_scapy.py
@@ -1,10 +1,14 @@
 from scapy.all import *
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import display
+
 
 # Open the pcap file
-pkts = rdpcap(r'hack\test.pcap')
-df = pandas.DataFrame()
+pkts = rdpcap(r'test.pcap')
+df = pd.DataFrame()
 data_list = []
+
 
 def get_packet_layers(packet):
     counter = 0
@@ -16,10 +20,11 @@ def get_packet_layers(packet):
         yield layer
         counter += 1
 
+
 pkt: Packet
 for pkt in pkts:
-    data = {"src": pkt.getlayer(Ether).src,
-            "dst": pkt.getlayer(Ether).dst,
+    data = {"src": None,
+            "dst": None,
             "src_ip": None,
             "dst_ip": None,
             "src_port": None,
@@ -33,19 +38,34 @@ for pkt in pkts:
     layer: Packet
     for layer in get_packet_layers(pkt):
         layer_name = layer.name
-        if layer_name is not None:
-            data["protocols"].append(layer_name)
-            
-            payload = Packet()
-            payload = layer.show(dump=True)
-            # print(payload)
-            if payload:
-                data["payloads"].append(payload)
-
-    data_list.append(data)
+        if layer_name and layer:
+            if hasattr(layer, 'qd'):
+                if layer_name == "DNS" and hasattr(layer.qd, 'qname'):
+                    if layer.qd.qname:
+                        data["payloads"].append(layer.qd.qname)
+                        print(data["payloads"])
+            # data["protocols"].append(layer_name)
+            #
+            # payload = Packet()
+            # payload = layer.show(dump=True)
+            # # print(payload)
+            # if payload:
+            #     data["payloads"].append(payload)
+    if data["payloads"]:
+        data_list.append(data["payloads"])
     
-df = pandas.DataFrame(data_list)
-print(len(df))
+df = pd.DataFrame(data=data_list)
+ndf = df.apply(pd.Series.value_counts)
+ndf.columns = ['Packet count']
 
-# Close the pcap file
+ndf.plot.bar()
+plt.show()
+
+print("Top 5 DNS (or more if duplicates): ")
+print(ndf.nlargest(n=5, columns='Packet count', keep='all'))
+
+
+# Close the pcap file .nlargest(5, keep='all')
 # pkts.close()
+# "src": pkt.getlayer(Ether).src,
+#             "dst": pkt.getlayer(Ether).dst
