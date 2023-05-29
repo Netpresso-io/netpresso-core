b5f55ba1a6c35119ab54c17cca5b71987c3ce864
Omri Yoffe <omri.yoffe@sap.com>
2023-05-29 17:55:44 +0300

    add bandwidth usage table

--- packet_server/packet_server.py
+++ packet_server/packet_server.py
@@ -1,12 +1,24 @@
 from flask import Flask, request
 from utils.packet_processing import PacketUtils
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/top-dns")
-def home():
-    return PacketUtils.get_top_dns(request.args.get("file_name"), int(request.args.get("packet_amount")))
+def top_dns():
+    packet_amount = request.args.get("packet_amount")
+    if packet_amount is None:
+        packet_amount = 7500
+    return PacketUtils.get_top_dns(request.args.get("file_name"), int(packet_amount))
+
+
+@app.route("/bandwidth-usage")
+def bandwidth_usage():
+    packet_amount = request.args.get("packet_amount")
+    if packet_amount is None:
+        packet_amount = 7500
+    return PacketUtils.get_bandwidth_usage(request.args.get("file_name"), int(packet_amount))
 
 
 if __name__ == "__main__":
