9a481153a03eee0512d20033cc2d2b5188bae924
Omri Yoffe <63257644+omriyo@users.noreply.github.com>
2023-07-31 22:49:20 +0300

    Deployment (#16)

--- packet_server/packet_server.py
+++ packet_server/packet_server.py
@@ -9,7 +9,7 @@ CORS(app)
 def top_dns():
     packet_amount = request.args.get("packet_amount")
     if packet_amount is None:
-        packet_amount = 50000
+        packet_amount = 7500
     return PacketUtils.get_top_dns(request.args.get("file_name"), int(packet_amount))
 
 
@@ -17,21 +17,9 @@ def top_dns():
 def bandwidth_usage():
     packet_amount = request.args.get("packet_amount")
     if packet_amount is None:
-        packet_amount = 50000
+        packet_amount = 7500
     return PacketUtils.get_bandwidth_usage(request.args.get("file_name"), int(packet_amount))
-
-
-@app.route("/dashboard")
-def dashboard():
-    packet_amount = request.args.get("packet_amount")
-    if packet_amount is None:
-        packet_amount = 150000
-    packet_list = PacketUtils.get_packets(request.args.get("file_name"), int(packet_amount))
-    res = {
-        "bandwidth": PacketUtils.calculate_bandwidth_usage(packet_list, PacketUtils.get_endpoints(packet_list)),
-        "top_dns": PacketUtils.process_dns_packets(packet_list)
-    }
-    return res
-
+  
+  
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
