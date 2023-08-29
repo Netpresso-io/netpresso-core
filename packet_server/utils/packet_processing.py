54e4daffbcaf945f92dea0d8933ba6a11d90e715
talbeja <talbeja05@gmail.com>
2023-08-29 20:32:06 +0300

    fixes

--- packet_server/utils/packet_processing.py
+++ packet_server/utils/packet_processing.py
@@ -9,7 +9,7 @@ import socket
 import threading
 
 packet_buff = []
-ethernet_interface = "eth2"  # Ruby needs to make sure this is the interface name
+ethernet_interface = "Wi-Fi"  # Ruby needs to make sure this is the interface name
 dns_queue = queue.Queue()
 bandwidth_queue = queue.Queue()
 
@@ -17,11 +17,11 @@ bandwidth_queue = queue.Queue()
 def process_dns_packets(packet_list):
     dns_count = Counter()
     for pkt in packet_list:
-        if DNS in pkt.layers() and pkt[DNS].an is not None:
-            for dns in pkt[DNS].an:
+        if DNS in pkt[0].layers() and pkt[0][DNS].an is not None:
+            for dns in pkt[0][DNS].an:
                 if dns.type == 1 or dns.type == 28 \
-                        and pkt[DNS].getlayer("DNSQR") is not None:
-                    domain_name = pkt[DNS].getlayer("DNSQR").qname.decode('utf-8')
+                        and pkt[0][DNS].getlayer("DNSQR") is not None:
+                    domain_name = pkt[0][DNS].getlayer("DNSQR").qname.decode('utf-8')
                     dns_count[domain_name] += 1
     return dns_count.most_common(5)
 
